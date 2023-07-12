6f8b3616a505731df9543166ce5803c0df74e15f
Meawhung <meawhung@gmail.com>
2023-07-13 00:29:22 +0900

    set file space

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,13 +1,40 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-  'strapi::security',
+  "strapi::errors",
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "wonwon-strapi-file.sgp1.digitaloceanspaces.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "wonwon-strapi-file.sgp1.digitaloceanspaces.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
